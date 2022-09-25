571afb927474c7591f265fc1d7bf992c6ca2a7a3
Ty Everett <ty@tyweb.us>
2022-09-24 21:28:05 -0700

    Support cool bytes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {
   TextField,
   Button,
   Typography,
-  Tabs,
-  Tab,
-  LinearProgress
+  Checkbox,
+  LinearProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
 } from '@material-ui/core'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -21,7 +25,6 @@ const useStyles = makeStyles(style, {
 })
 export default () => {
   const classes = useStyles()
-  const [tabIndex, setTabIndex] = useState(0)
   const [serverURL, setServerURL] = useState(
     window.location.host.startsWith('localhost')
       ? 'http://localhost:3002'
@@ -30,6 +33,11 @@ export default () => {
         : 'https://byte-shop.babbage.systems'
   )
   const [numberOfBytes, setNumberOfBytes] = useState(14)
+  const [cool, setCool] = useState(false)
+  const [coolcertURL, setCoolcertURL] = useState(
+    'https://coolcert-ui.babbage.systems'
+  )
+  const [coolcertModalOpen, setCoolcertModalOpen] = useState(false)
   const [results, setResults] = useState('')
   const [loading, setLoading] = useState(false)
   const [uploadProgress, setUploadProgress] = useState(0)
@@ -45,11 +53,11 @@ export default () => {
       }
       const invoiceResult = await invoice({
         numberOfBytes,
+        cool,
         config: {
           byteshopURL: serverURL
         }
       })
-      console.log('App():invoiceResult:', invoiceResult)
       const payResult = await pay({
         config: {
           byteshopURL: serverURL
@@ -59,17 +67,21 @@ export default () => {
         recipientPublicKey: invoiceResult.identityKey,
         amount: invoiceResult.amount
       })
-      console.log('App():payResult:', payResult)
       setResults({
         bytes: payResult.bytes,
         note: payResult.note
       })
     } catch (e) {
-      console.error(e)
-      if (e.response && e.response.data && e.response.data.description) {
-        toast.error(e.response.data.description)
+      if (e.code === 'ERR_NOT_COOL_ENOUGH') {
+        setCoolcertURL(e.coolcertURL)
+        setCoolcertModalOpen(true)
       } else {
-        toast.error(e.message)
+        console.error(e)
+        if (e.response && e.response.data && e.response.data.description) {
+          toast.error(e.response.data.description)
+        } else {
+          toast.error(e.message)
+        }
       }
     } finally {
       setLoading(false)
@@ -80,77 +92,70 @@ export default () => {
   return (
     <div className={classes.content_wrap}>
       <ToastContainer />
-      <center>
-        <Typography variant='h4'>Byteshop UI</Typography>
+      <Typography align='center' variant='h4' paragraph>
+        The Byte Shop
+      </Typography>
+      <form onSubmit={handleBuy}>
         <br />
         <br />
-        <Tabs
-          onChange={(e, v) => setTabIndex(v)}
-          value={tabIndex}
-          indicatorColor='primary'
-          textColor='primary'
-          variant='fullWidth'
-        >
-           <Tab label='Buy bytes' />
-        </Tabs>
-      </center>
-      {tabIndex === 0 && (
-        <form onSubmit={handleBuy}>
+        <Typography variant='h5'>Server URL</Typography>
+        <Typography paragraph>
+          Enter the URL of the Byteshop server to interact with
+        </Typography>
+        <TextField
+          fullWidth
+          variant='outlined'
+          label='Server URL'
+          value={serverURL}
+          onChange={e => setServerURL(e.target.value)}
+        />
+        <br />
+        <br />
+        <br />
+        <Typography paragraph>
+          Number of bytes (10 or more)
+        </Typography>
+        <TextField
+          fullWidth
+          variant='outlined'
+          label='Number of bytes'
+          value={numberOfBytes}
+          onChange={e => setNumberOfBytes(e.target.value)}
+        />
+        <Typography paragraph>
+          <Checkbox
+            value={cool}
+            onChange={() => setCool(c => !c)}
+          /> Request Cool Bytes
+        </Typography>
+        <center className={classes.broadcast_wrap}>
+          <Button
+            variant='contained'
+            color='primary'
+            size='large'
+            type='submit'
+            disabled={loading}
+            startIcon={<Buy />}
+          >
+            Buy
+          </Button>
           <br />
           <br />
-          <Typography variant='h5'>Server URL</Typography>
-          <Typography paragraph>
-            Enter the URL of the Byteshop server to interact with
-          </Typography>
-          <TextField
-            fullWidth
-            variant='outlined'
-            label='Server URL'
-            value={serverURL}
-            onChange={e => setServerURL(e.target.value)}
-          />
-          <br />
-          <br />
-          <br />
-          <Typography paragraph>
-            Enter number of bytes to be purchased > 10
-          </Typography>
-          <TextField
-            fullWidth
-            variant='outlined'
-            label='Number of bytes'
-            value={numberOfBytes}
-            onChange={e => setNumberOfBytes(e.target.value)}
-          />
-          <center className={classes.broadcast_wrap}>
-            <Button
-              variant='contained'
-              color='primary'
-              size='large'
-              type='submit'
-              disabled={loading}
-              startIcon={<Buy />}
-            >
-              Buy
-            </Button>
-            <br />
-            <br />
-            {loading && (
-              <LinearProgress
-                variant={uploadProgress === 0 ? 'indeterminate' : 'determinate'}
-                value={uploadProgress === 0 ? undefined : uploadProgress}
-              />
-            )}
-            {results && (
-              <div>
-                <Typography variant='h4'>Success!</Typography>
-                <Typography><b>Your bytes:</b>{' '}{results.bytes}</Typography>
-                <Typography><b>Note:</b>{' '}{results.note}</Typography>
-              </div>
-            )}
-          </center>
-        </form>
-      )}
+          {loading && (
+            <LinearProgress
+              variant={uploadProgress === 0 ? 'indeterminate' : 'determinate'}
+              value={uploadProgress === 0 ? undefined : uploadProgress}
+            />
+          )}
+          {results && (
+            <div>
+              <Typography variant='h4'>Success!</Typography>
+              <Typography><b>Your bytes:</b>{' '}{results.bytes}</Typography>
+              <Typography><b>Note:</b>{' '}{results.note}</Typography>
+            </div>
+          )}
+        </center>
+      </form>
       <br />
       <Typography align='center'>
         View the <a href='https://github.com/p2ppsr/byte-shop-ui'>GitHub Repo</a> for this site
@@ -159,6 +164,33 @@ export default () => {
       <Typography align='center'>
         Made with <a href='https://projectbabbage.com'>www.ProjectBabbage.com</a> tools :)
       </Typography>
+      {/* Dialog for dealing with CoolCert */}
+      <Dialog
+        open={coolcertModalOpen}
+        onClose={() => setCoolcertModalOpen(false)}
+      >
+        <DialogTitle>
+          Cool Person Certificate Required
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Before buying Cool Bytes, you need a Cool Person Certificate from the CoolCert CA. Please visit the following URL to obtain one:
+          </DialogContentText>
+          <DialogContentText>
+            <a href={coolcertURL} target='_blank' rel='noopener noreferrer'>{coolcertURL}</a>
+          </DialogContentText>
+          <DialogContentText>
+            When you are finished, return to this page and try again.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => setCoolcertModalOpen(false)}
+          >
+            Close & Try Again
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
