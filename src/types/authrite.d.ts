01115bf95c82b48127998cf6c2890aaa16ff9745
bob@babbageproject.com <bob@babbageproject.com>
2024-07-30 14:55:19 +0100

    removed those .d.ts files that were not required

--- src/types/authrite.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'authrite' {
-  export class AuthriteClient {
-    constructor(url: string)
-    createSignedRequest(endpoint: string, payload: any): Promise<any>
-  }
-}
--- src/types/use-async-effect.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'use-async-effect' {
-    import { EffectCallback, DependencyList } from 'react'
-  
-    export default function useAsyncEffect(
-      effect: () => Promise<void | ReturnType<EffectCallback>>,
-      deps?: DependencyList
-    ): void
-  }
-  
